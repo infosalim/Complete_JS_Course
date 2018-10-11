288fa6c82f57c90c35f209412e6e55041940c673
Md Salim Hossain <salimh5757@gmail.com>
2018-10-11 23:23:18 +0600

    API call

--- 8-Forkify/src/js/index.js
+++ 8-Forkify/src/js/index.js
@@ -1,4 +1,20 @@
 // Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module! called test.js! Variable x is ${x}...`);
+// 2952c5405ee286946b61857b7d009709
+// https://www.food2fork.com/api/search
+
+import axios from 'axios';
+
+async function getResult(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '2952c5405ee286946b61857b7d009709';
+    try {
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+getResult('pizza');
